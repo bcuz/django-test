699afc5f22140c00c66e4466f0189c2fab290537
adam <bcuz@users.noreply.github.com>
2020-01-10 10:54:32 -0500

    db stuff

--- adam/settings.py
+++ adam/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,12 +76,15 @@ WSGI_APPLICATION = 'adam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES = {}
+# cant seem to get this to not work (via editing DATABASE_URL in .env), but moving on for now
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
